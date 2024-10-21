b5d1737cd76cf872de95e58a38f3084af951bfb7
jj12342s <101233996+jj12342s@users.noreply.github.com>
2024-10-21 13:36:27 +0000

    Prototype Finished

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SysCom
+
+This program will allow the user to submit messages to a file. Other instances of this program will pick-up on these messages and display it to the other user.
+It's basically a same-system chat room. 
+
---
+++ main.py
@@ -0,0 +1,44 @@
+from threading import Thread 
+import csv 
+from colorama import Fore, Back, Style
+def print_diff(current, previous):
+    if current != previous:
+        
+            for i in reversed(range(len(current) - len(previous))):
+                if not i:
+                    continue
+                if not current[-i -1][0] == user:
+                    print(Back.BLUE + current[-i -1][0] + Style.RESET_ALL)
+                    print(current[-i -1][1] + "\n")
+def refresh_incomming():
+    global user
+    previous_text = []
+    while True:
+        current_text = []
+        with open("room.csv", "r") as file:
+            file = csv.reader(file)
+            for line in file:
+                current_text.append(line)
+
+            print_diff(current_text, previous_text)
+            previous_text = current_text
+
+def take_input():
+    global user 
+    while True:
+        with open("room.csv", "a") as file:
+            file = csv.writer(file)
+            message = input()
+            file.writerow([user, message])
+            
+
+
+if __name__ == "__main__":
+    global user
+    user = input("What is your name?")
+    t1 = Thread(target=refresh_incomming)
+    t2 = Thread(target=take_input)
+    
+    t2.start()
+    t1.start()
+    
